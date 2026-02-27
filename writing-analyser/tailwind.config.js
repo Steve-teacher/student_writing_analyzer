e8f7b5dbdac7cb9317e41a96bd28121b29272f7c
Steve-teacher <sgerber@sd74.org>
2026-02-27 12:57:46 -0600

    Update tailwind.config.js

--- writing-analyser/tailwind.config.js
+++ writing-analyser/tailwind.config.js
@@ -1,6 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+module.exports = {};
